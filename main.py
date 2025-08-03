19dc17b2a442386a13e5275780e6ddee09d18e9d
Kredigen <phlpdale@gmail.com>
2025-08-03 21:41:31 +0200

    added lives

--- main.py
+++ main.py
@@ -27,12 +27,12 @@ def main():
     # It creates an actual Player object and stores it in the variable 'player'
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
-    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        global PLAYER_LIFE
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -40,7 +40,11 @@ def main():
         updatable.update(dt)
         for aste in asteroids:
             if aste.check_collision(player):
-                sys.exit("Game over!")
+                if PLAYER_LIFE > 0:
+                    PLAYER_LIFE -= 1
+                    print(PLAYER_LIFE)
+                elif PLAYER_LIFE == 0 :
+                    sys.exit("Game over!")
         for aste in asteroids:
             for shot in shots:
                 #print(f"Checking collision between asteroid at {aste.position} and shot at {shot.position}")
